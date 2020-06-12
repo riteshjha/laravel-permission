6f853d91dbd0e042ee7b9695003eb07404953d6a
Ritesh <mailrkj@gmail.com>
2020-06-12 23:34:27 +0545

    added env in config

--- src/config/permission.php
+++ src/config/permission.php
@@ -3,27 +3,27 @@
 return [
 
     // set permission level ('account' or 'owner') when you select account level then be sure you have account model 
-    'level' => 'owner', 
+    'level' => env('PERMISSION_LEVEL', 'owner'), 
 
-    'disableAbilityGroup' => false,
+    'disableAbilityGroup' => env('PERMISSION_DISABLE_ABILITY_GROUP', false),
 
     'model' => [      
-        'namespace' => 'App',
+        'namespace' => env('PERMISSION_MODEL_NAMESPACE','App'),
 
-        'user' => "User", //user model
+        'user' => env('PERMISSION_MODEL_USER','User'), //user model
 
-        'role' => "Role", //role model
+        'role' => env('PERMISSION_MODEL_ROLE','Role'), //role model
 
-        'ability' => "Rkj\Permission\Models\Ability", //Ability model that contain abilities
+        'ability' => env('PERMISSION_MODEL_ABILITY', 'Rkj\Permission\Models\Ability'), //Ability model that contain abilities
 
-        'account' => "Account"  //account model, if using account level check
+        'account' => env('PERMISSION_MODEL_ACCOUNT','Account'), //account model, if using account level check
     ],
 
     'role' => [
-        'superAdmin' => 'superadmin', //specify role name which has all permissions
+        'superAdmin' => env('PERMISSION_ROLE_SUPPERADMIN','superadmin'), //specify role name which has all permissions
     ],
 
-    'adminPrefix' => 'admin', //prefix used for admin routes
+    'adminPrefix' => env('PERMISSION_ADMIN_ROUTE_PREFIX','admin'), //prefix used for admin routes
 
-    'itemPerPage' => 15
+    'itemPerPage' => env('PERMISSION_ITEM_PERPAGE',15),
 ];
